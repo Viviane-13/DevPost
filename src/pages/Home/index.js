59c0355c96b3c213bdb27d7c965d4f9a4a4d697f
Viviane <oviviane772@gmail.com>
2022-07-16 19:57:37 -0300

    Buscando posts

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useCallback } from 'react';
 
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
 
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import Feather from 'react-native-vector-icons/Feather';
+import { AuthContext } from '../../contexts/auth';
+
+import firestore from '@react-native-firebase/firestore';
 
 import { Container, ButtonPost, ListPosts } from './styles';
 import { Header } from '../../components/Header';
@@ -11,17 +14,52 @@ import { Header } from '../../components/Header';
 export function Home() {
   const navigation = useNavigation();
 
-  const [posts, setPosts] = useState([
-    { id: 1, content: 'Teste123' },
-    { id: 2, content: 'Segundo Post' },
-    { id: 3, content: 'Terceiro Post' },
-  ]);
+  const { user } = useContext(AuthContext);
+
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useFocusEffect(
+    useCallback(() => {
+      let isActive = true;
+      function fetchPosts() {
+        firestore()
+          .collection('posts')
+          .orderBy('created', 'desc')
+          .limit(5)
+          .get()
+          .then((snapshort) => {
+            if (isActive) {
+              setPosts([]);
+              const postList = [];
+
+              snapshort.docs.map((posts) => {
+                postList.push({ ...posts.data(), id: posts.id });
+              });
+              setPosts(postList);
+              setLoading(false);
+            }
+          });
+      }
+      fetchPosts();
+      return () => {
+        isActive = false;
+      };
+    }, [])
+  );
 
   return (
     <Container>
       <Header />
-
-      <ListPosts data={posts} renderItem={({ item }) => <Text>Teste</Text>} />
+      {loading ? (
+        <View
+          style={{ flex: 1, justifyContent: 'center', alignContent: 'center' }}
+        >
+          <ActivityIndicator size={50} color="#E52246" />
+        </View>
+      ) : (
+        <ListPosts data={posts} renderItem={({ item }) => <Text>Teste</Text>} />
+      )}
 
       <ButtonPost
         activeOpacity={0.8}
