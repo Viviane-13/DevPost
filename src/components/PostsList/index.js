63e89afb1d7b660fb5e2cde9d34f2767a858dafe
Viviane <oviviane772@gmail.com>
2022-07-20 16:37:46 -0300

    Realizando Like

--- src/components/PostsList/index.js
+++ src/components/PostsList/index.js
@@ -14,12 +14,51 @@ import {
 
 import { formatDistance } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
-
+import firestore from '@react-native-firebase/firestore';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export function PostsList({ data, userId }) {
   const [likePost, setLikePost] = useState(data?.likes);
 
+  async function handleLikePost(id, likes) {
+    const docId = `${userId}_${id}`;
+
+    //Checar se post já foi curtido
+    const doc = await firestore().collection('likes').doc(docId).get();
+
+    if (doc.exists) {
+      //Já curtiu o post, remover o like
+      await firestore()
+        .collection('posts')
+        .doc(id)
+        .update({
+          likes: likes - 1,
+        });
+      await firestore()
+        .collection('likes')
+        .doc(docId)
+        .delete()
+        .then(() => {
+          setLikePost(likes - 1);
+        });
+      return;
+    }
+    //Colocar o like no post
+    await firestore().collection('likes').doc(docId).set({
+      postId: id,
+      userId: userId,
+    });
+
+    await firestore()
+      .collection('posts')
+      .doc(id)
+      .update({
+        likes: likes + 1,
+      })
+      .then(() => {
+        setLikePost(likes + 1);
+      });
+  }
   function formatTimePost() {
     const datePost = new Date(data.created.seconds * 1000);
 
@@ -40,7 +79,7 @@ export function PostsList({ data, userId }) {
         <Content>{data?.content}</Content>
       </ContentView>
       <Actions>
-        <LikeButton>
+        <LikeButton onPress={() => handleLikePost(data.id, likePost)}>
           <Like>{likePost === 0 ? '' : likePost}</Like>
           <Icon
             name={likePost === 0 ? 'heart-plus-outline' : 'cards-heart'}
