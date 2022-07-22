20aa02338b0749d7973e89457918ec2df189cab6
Viviane <oviviane772@gmail.com>
2022-07-22 20:37:17 -0300

    Search

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { View, Text } from 'react-native';
 
+import Feather from 'react-native-vector-icons/Feather';
+
+import { Container, AreaInput, Input, List } from './styles';
+
 export function Search() {
+  const [input, setInput] = useState('');
+  const [users, setUsers] = useState([]);
   return (
-    <View>
-      <Text>Tela Search</Text>
-    </View>
+    <Container>
+      <AreaInput>
+        <Feather name="search" size={20} color="#E52246" />
+        <Input
+          placeholder="Procurando alguém? "
+          value={input}
+          onChangeText={(text) => setInput(text)}
+          placeholderTextColor="#353840"
+        />
+      </AreaInput>
+    </Container>
   );
 }
