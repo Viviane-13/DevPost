c5e8a774798b42d6f9ae6d6167a9563b05195405
Viviane <oviviane772@gmail.com>
2022-07-23 15:38:46 -0300

    Modal do perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,13 @@
 import React, { useContext, useState } from 'react';
 
-import { View, Text } from 'react-native';
+import { View, Text, Modal, Platform } from 'react-native';
 import { AuthContext } from '../../contexts/auth';
 
 import { Header } from '../../components/Header';
+
+import Feather from 'react-native-vector-icons/Feather';
+import firestore from '@react-native-firebase/firestore';
+
 import {
   Container,
   Name,
@@ -13,6 +17,9 @@ import {
   UploadButton,
   UploadText,
   Avatar,
+  ModalContainer,
+  ButtonBack,
+  Input,
 } from './styles';
 
 export function Profile() {
@@ -21,8 +28,14 @@ export function Profile() {
   const [name, setName] = useState(user?.nome);
   const [url, setUrl] = useState(null);
 
+  const [open, setOpen] = useState(false);
+
   async function handleSignOut() {
     await signOut();
+  }
+  //Atualizar o perfil
+  async function updateProfile() {
+    alert('Teste');
   }
   return (
     <Container>
@@ -39,12 +52,28 @@ export function Profile() {
       )}
       <Name>{user?.nome}</Name>
       <Email>{user?.email}</Email>
-      <Button bg="#428cfd">
+      <Button bg="#428cfd" onPress={() => setOpen(true)}>
         <ButtonText color="#fff">Atualizar Perfil</ButtonText>
       </Button>
       <Button bg="#ddd" onPress={handleSignOut}>
         <ButtonText color="#353840">Sair</ButtonText>
       </Button>
+      <Modal visible={open} animationType="slide" transparent={true}>
+        <ModalContainer behavior={Platform.OS === 'android' ? '' : 'padding'}>
+          <ButtonBack onPress={() => setOpen(false)}>
+            <Feather name="arrow-left" size={25} color="#121212" />
+            <ButtonText color="#121212">Voltar</ButtonText>
+          </ButtonBack>
+          <Input
+            placeholder={user?.nome}
+            value={name}
+            onChangeText={(text) => setName(text)}
+          />
+          <Button bg="#428cfd" onPress={updateProfile}>
+            <ButtonText color="#fff">Salvar</ButtonText>
+          </Button>
+        </ModalContainer>
+      </Modal>
     </Container>
   );
 }
